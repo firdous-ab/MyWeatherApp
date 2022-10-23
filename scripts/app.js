41382a10f39e8c95baf8d1e01ad61f08e4b77d3d
firdous-ab <fridausb2004@gmail.com>
2022-10-23 18:47:28 +0100

    first commit

---
+++ scripts/app.js
@@ -0,0 +1,1 @@
+// this contains all the js that will be used for DOM manipulation
---
+++ scripts/forecast.js
@@ -0,0 +1,34 @@
+//this contain js that will be used to interact with the weather API
+
+const key = "BED3GYKBcL5fkmnublzYTjPpGUN4gm2O";
+
+//get weather information
+const getWeather = async (id) => {
+  const base = "http://dataservice.accuweather.com/currentconditions/v1/";
+  const query = `${id}?apikey=${key}`;
+
+  const response = await fetch(base + query);
+  const data = await response.json();
+
+  return data[0];
+};
+
+// get city information
+const getCity = async (city) => {
+  const base = "http://dataservice.accuweather.com/locations/v1/cities/search";
+  const query = `?apikey=${key}&q=${city}`;
+
+  const response = await fetch(base + query);
+  const data = await response.json();
+
+  return data[0];
+};
+
+getCity("manchester")
+  .then((data) => {
+    return getWeather(data.Key);
+  })
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((err) => console.log(err));
